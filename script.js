7131850893c6bb842902c56e09011d11f4ce2218
Aldo Solares <aldo_solares@outlook.com>
2025-03-26 18:18:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-// Función que redirige a la página si.html al hacer clic en el botón sí.
+// Función que redirige a la página si.html al hacer clic en el botón "Sí".
 function nextPage() {
   window.location.href = 'si.html'
 }
 
-// Función que mueve el botón "No" a una posición aleatoria dentro de la ventana del navegador.
+// Función que mueve el botón "No" a una posición aleatoria dentro de la ventana del navegador,
+// asegurando que la nueva posición sea significativamente diferente a la anterior.
 function moveButton() {
   const Boton_N = document.getElementById('Boton_no')
   const maxX = window.innerWidth - Boton_N.clientWidth
   const maxY = window.innerHeight - Boton_N.clientHeight
-  const randomX = Math.random() * maxX
-  const randomY = Math.random() * maxY
+
+  let newX, newY
+  let threshold = 100 // Distancia mínima de cambio para evitar movimientos pequeños.
+
+  do {
+    newX = Math.random() * maxX
+    newY = Math.random() * maxY
+  } while (Math.abs(newX - Boton_N.offsetLeft) < threshold && Math.abs(newY - Boton_N.offsetTop) < threshold)
+
   Boton_N.style.position = 'absolute'
-  Boton_N.style.left = `${randomX}px`
-  Boton_N.style.top = `${randomY}px`
+  Boton_N.style.left = `${newX}px`
+  Boton_N.style.top = `${newY}px`
 }
 
 // Asigna los event listeners a los botones.
